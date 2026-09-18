bbbf1bee391935e2673987cfb712a1cc7e387154
juanclarinete27 <juanclarinete2007@gmail.com>
2026-09-18 18:52:45 -0300

    Fix Mupen64Plus libpng for Xcode 16

--- Cores/Mupen64Plus/png/pngpriv.h
+++ Cores/Mupen64Plus/png/pngpriv.h
@@ -450,11 +450,11 @@
 #  if (defined(__MWERKS__) && defined(macintosh)) || defined(applec) || \
     defined(THINK_C) || defined(__SC__) || defined(TARGET_OS_MAC)
    /* We need to check that <math.h> hasn't already been included earlier
-    * as it seems it doesn't agree with <fp.h>, yet we should really use
-    * <fp.h> if possible.
+    * as it seems it doesn't agree with <math.h>, yet we should really use
+    * <math.h> if possible.
     */
 #    if !defined(__MATH_H__) && !defined(__MATH_H) && !defined(__cmath__)
-#      include <fp.h>
+#      include <math.h>
 #    endif
 #  else
 #    include <math.h>
